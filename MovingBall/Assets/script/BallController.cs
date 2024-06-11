e34d39a0034d6b817451a621a1516945ac90834e
ankitrj3 <ankit.12000777@lpu.in>
2024-06-11 21:36:46 +0530

    Moving ball in Unity  by using c#

---
+++ MovingBall/Assets/script/BallController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    public float speed;
+    void Update()
+    {
+        float horizontalinput = Input.GetAxis("Horizontal");
+        float verticallinput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalinput, 0.0f, verticallinput) * speed * Time.deltaTime;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(movement,ForceMode.Impulse);
+
+    }
+}
