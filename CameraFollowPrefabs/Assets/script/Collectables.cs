0df7166277fd13ed0ea1db01502cf519c12b584d
ankitrj3 <ankit.12000777@lpu.in>
2024-06-17 20:03:26 +0530

    Camera Focus Prefabs in Unity by using c#

---
+++ CameraFollowPrefabs/Assets/script/Collectables.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Invoke("DestroyCollectables",2f);
+            
+            Debug.Log("ball collided with coin");
+        }
+    }
+
+    void DestroyCollectables()
+    {
+        Destroy(this.gameObject);
+    }
+}
